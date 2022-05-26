11be4704cc9936255502d9f0d66a9dba611e9d61
quywrh <98122356+quywrh@users.noreply.github.com>
2022-05-26 14:58:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def input_f():
     return n
 def x3_1n(n):
     return collatz(n*3+1)
+def collatz(n):
+    result = [n]
+    if n == 1:
+        pass
+    elif n % 2 == 0:
+        result.extend(x2(n))
+    else:
+        result.extend(x3_1(n))
+    return result
